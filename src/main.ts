75ed117dfb667510edaa88c29d7067b565b1e515
Ariel <arielah1997@gmail.com>
2024-03-08 02:29:12 +0200

    fix: port is from env

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
